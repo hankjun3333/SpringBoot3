10c2b15221b8ba94290d89ebbb41c48c0604183c
hankjun3333 <hankjun3333@naver.com>
2023-07-26 17:46:17 +0900

    lesson

--- 직접 실습 해본것들/MVCpatternSample/src/main/java/com/example/demo/controller/ArticleController.java
+++ 직접 실습 해본것들/MVCpatternSample/src/main/java/com/example/demo/controller/ArticleController.java
@@ -3,30 +3,51 @@ package com.example.demo.controller;
 import com.example.demo.controller.entity.Article;
 import com.example.demo.dto.ArticleForm;
 import com.example.demo.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class ArticleController {
     @Autowired//Autowired를 쓰면 스프링 부트가 미리 생성해놓은 객체를 가져다가 연결해줌
     private ArticleRepository articleRepository;
+
     @GetMapping("/articles/new")
     public String newArticleForm(){
         return "articles/new";
     }
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form){
-        System.out.println(form.toString());
+        log.info(form.toString());
+        //System.out.println(form.toString());
+
         //1.DTO를 엔티티로 변환
         Article article = form.toEntity();
-        System.out.println(article.toString());
+        log.info(article.toString());
+        //System.out.println(article.toString());
+
         //2.리파지토리 로 엔티티를 DB에 저장
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
+        log.info(saved.toString());
+        //System.out.println(saved.toString());
         // repository/ ArticleRepository 인터페이스를 정의하면
         //JPA에서 제공하는 CRUDRepository 인터페이스 상속해서 crud 메소드를 사용할 수 있음 save,findall 등등...
         return "";
     }
+    @GetMapping("articles/{id}")
+    public String show(@PathVariable Long id, Model model){
+        log.info("id = " + id);
+        //1. id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        // 2. 모델에 데이터 등록
+        model.addAttribute("article","articleEntity");
+        //3. 뷰 페이지 반환하기
+        return "articles/show";
+    }
 }
+
--- 직접 실습 해본것들/MVCpatternSample/src/main/java/com/example/demo/controller/entity/Article.java
+++ 직접 실습 해본것들/MVCpatternSample/src/main/java/com/example/demo/controller/entity/Article.java
@@ -1,17 +1,20 @@
 package com.example.demo.controller.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Article {
     //이 어노테이션이 붙은 클래스를 기반으로 DB에
     // 테이블이 생성됨 테이블 이름은 클래스 이름과 동일하게 생김
-    //DTO 코드 작성할때 처럼 title, content 필드선언 두 필드를 column도 선언해줌 각각
+    //DTO 코드 작성할때 처럼 title, content 필드선언 두 필드를 column 도 선언해줌 각각
     @Id // 이게 엔티티의 대표값임 이게 대표값이라햇다 분명
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column
     private String title;
@@ -20,20 +23,8 @@ public class Article {
     //그리고 엔티티의 대표값을 설정해줘야함 그리고 그 대표값을 id 라고 어노테이션 붙여줌
     // 그리고 필드 선언하면 값을 초기화 해줘야겟지?
 
-//toString은 값이 잘 들어왔나 확인용으로 메소드 추가해주는 거임
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
+//toString 은 값이 잘 들어왔나 확인용으로 메소드 추가해주는 거임
 
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
+
 
 }
